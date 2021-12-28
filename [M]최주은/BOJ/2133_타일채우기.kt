09873c19a2b8ea55a8bc2082a4232b56ef7e9bb2
Jueun Choi <ilutoo@naver.com>
2021-12-29 00:37:57 +0900

    Create 2133_타일채우기.kt

---
+++ [M]최주은/BOJ/2133_타일채우기.kt
@@ -0,0 +1,25 @@
+package boj.`2133_타일채우기`
+
+val N = readLine()!!.toInt()
+val d = Array(1001) { 0 }
+
+fun main() {
+    // 홀수면 무조건 짝이 맞지 않아 0이다.
+    if(N%2==0) dp(N)
+    print(dp(N))
+}
+
+fun dp(n: Int) :Int {
+    if(n == 0) return 1
+    if(n == 1) return 0
+    if(n == 2) return 3
+    if(d[n] != 0) return d[n]
+
+    var res = 3 * dp(n-2)
+    for (i in 3..n){
+        if (i % 2 == 0) res += 2 * dp(n - i)
+    }
+
+    d[n] = res
+    return d[n]
+}
